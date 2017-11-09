96e8c8a1196d5ac4b5113e4d6d6e52acf6da8d1f
Sergio Díaz Real <sdiazr02@github.com>
2017-11-09 19:05:58 +0100

    Ejercicio6 terminado.
    Falta comentarlo únicamente

--- Ejercicios1-7/src/Ejercicio6/EscrituraXMLStAX.java
+++ Ejercicios1-7/src/Ejercicio6/EscrituraXMLStAX.java
@@ -2,6 +2,7 @@ package Ejercicio6;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.util.Scanner;
 
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
@@ -10,30 +11,47 @@ import javax.xml.stream.XMLStreamWriter;
 public class EscrituraXMLStAX {
 	public static final String FICHERO = "librosstax.xml";
 
-	public static void leerXML() {
+	public static String pideCadena(String cadena) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.print(cadena + ": ");
+		return entrada.nextLine();
+	}
+
+	public static void escribirXML() {
 
 		try {
 			FileOutputStream fos = new FileOutputStream(FICHERO);
 			XMLOutputFactory factory = XMLOutputFactory.newInstance();
-			XMLStreamWriter xmlStreamWriter = factory.createXMLStreamWriter(fos,"UTF-8");
-			xmlStreamWriter.writeStartDocument();
-			xmlStreamWriter.writeStartElement("libros");
+			XMLStreamWriter xmlStreamWriter = factory.createXMLStreamWriter(fos, "UTF-8");
 			
+			//ABRO DOCUMENTO
+			xmlStreamWriter.writeStartDocument();
+			//Abro nodo
+			xmlStreamWriter.writeStartElement("libros");
+
 			xmlStreamWriter.writeStartElement("libro");
 			xmlStreamWriter.writeAttribute("ISBN", "239-87-9964-088-4");
-			
+
 			xmlStreamWriter.writeStartElement("titulo");
 			xmlStreamWriter.writeCharacters("Acceso a Datos");
 			xmlStreamWriter.writeEndElement();
+
+			do {
+				xmlStreamWriter.writeStartElement("autor");
+				xmlStreamWriter.writeCharacters(pideCadena("Introduce el nombre del autor"));
+				xmlStreamWriter.writeEndElement();
+			} while (pideCadena("¿Otro autor?").equalsIgnoreCase("si"));
 			
-			xmlStreamWriter.writeStartElement("autor");
-			xmlStreamWriter.writeCharacters("Acceso a Datos");
+			xmlStreamWriter.writeStartElement("editorial");
+			xmlStreamWriter.writeCharacters("Garceta");
 			xmlStreamWriter.writeEndElement();
-			
-			xmlStreamWriter.writeStartElement("titulo");
-			xmlStreamWriter.writeCharacters("Acceso a Datos");
+
+			//Cierro nodo
 			xmlStreamWriter.writeEndElement();
+			//CIERRO DOCUMENTO
+			xmlStreamWriter.writeEndDocument();
 			
+			System.out.println("Fichero ---> "+FICHERO+" creado correctamente.");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- Ejercicios1-7/src/Ejercicio6/Principal.java
+++ Ejercicios1-7/src/Ejercicio6/Principal.java
@@ -3,8 +3,8 @@ package Ejercicio6;
 public class Principal {
 
 	public static void main(String[] args) {
-
-
+		EscrituraXMLStAX escrituraXMLStAX = new EscrituraXMLStAX();
+		escrituraXMLStAX.escribirXML();
 	}
 
 }
